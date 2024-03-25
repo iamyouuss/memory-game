5e232092ba358ccf92ba758869897b0603e0a51b
kiiraa <k.senane94@gmail.com>
2024-03-25 11:22:30 +0100

    ffgdmdkfjgidufu

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ card.addEventListener("click", function () {
     this.classList.toggle("background")
 })*/
 
-let grid = document.querySelector(".grid")
+let grid = document.querySelector(".grid") //lien des images pour les cartes//
 
 let links = [{
         id: 1,
@@ -44,12 +44,19 @@ let links = [{
     },
 ]
 
-let visuel = 'assets/default.png';
+let visuel = 'assets/default.png'; //lien de l'image par défaut pour le dos des cartes//
+
+let clickedcard = [] //tableau pour ajouter les deux cartes cliquées le tempos deles comparer//
+
+let tentatives = 0; //compteur pour le nombre de tentatives//
+
+let tentatives_block = document.querySelector(".tentative")
 
 
-function init() {
-    links = [...links, ...links]
-    links.sort(() => 0.5 - Math.random())
+console.log(tentatives_block)
+function init() { //fonction pour choisir au hasard une image à chaque clique//
+    links = [...links, ...links] //je double le tableau pour avoir 16 cartes//
+    links.sort(() => 0.5 - Math.random()) //sans 0.5 une seule dispositon possible//
 
     createcard()
 }
@@ -57,45 +64,83 @@ init()
 
 
 function createcard() {
-    let clickedcard = []
-    links.forEach(link => {
-        const img = document.createElement('img')
-        img.setAttribute('src', visuel)
-
-        img.classList.add('card')
-        grid.appendChild(img)
-
-        img.addEventListener("click", function () {
-            if (clickedcard.length < 16 && !clickedcard.includes(link.id)) {
-                img.setAttribute('src', link.src)
-                img.setAttribute('data-id', link.id)
 
 
-                clickedcard.push(link.id)
-            }
-            console.log(link.id)
-            console.log(clickedcard)
+    links.forEach(link =>
+        // j'attribue à chaque carte un élément html 'img' auquel j'ajoute un attribut src que je récupère dans la variable visuel (pour le dos de la carte)//
+        {
+            const img = document.createElement('img')
+            img.setAttribute('src', visuel)
 
-            if (clickedcard.length == 2 && clickedcard[0] != clickedcard[1]) {
-                setInterval(() => {
-                    img.setAttribute('src', visuel)
-                }, 1000)
+            img.classList.add('card') //je leur donne une taille en leur rattachant la class '.card' déjà existante dans le css//
+            grid.appendChild(img) //j'ajoute les img(child) au grid(parent)//
 
-                clickedcard = []
 
-            }
 
-            //     img.setAttribute('src', visuel)
-            //     console.log("hello")
-            // }
-            // else{
-            //     clickedcard.forEach(function(card)
-            //     {
-            //         img.setAttribute('src', visuel)
-            //     });
-            //     clickedcard = []
-            // }
-        })
+            img.addEventListener("click", function c() {
+                tentatives++;
+                tentatives_block.textContent = tentatives
+                
+                if (clickedcard.length < 2) {
+                    img.setAttribute('src', link.src) //lorsqu'une carte est cliquée je lui attribue une image grâce au tableau links et à la fonction init//
+                    // this.removeEventListener('click', c)
+                    // img.setAttribute('data-id', link.id)
 
-    })
-}
+                    clickedcard.push({
+                        id: link.id,
+                        img: img
+                    })
+                    // console.log(link.id)
+                    console.log(clickedcard)
+
+                    if(clickedcard.length===2)
+                    
+
+                    if (clickedcard.length === 2 && clickedcard[0].id == clickedcard[1].id) {
+                        clickedcard.forEach(card => {
+                            card.img.classList.add('matched');
+                        })
+                          //  img.removeEventListener('click', c);
+
+                        if (document.querySelectorAll('.matched').length === links.length) {
+                            // Toutes les paires ont été trouvées, afficher le message de félicitations
+                            setTimeout(() => {
+                                alert('Félicitations, vous avez trouvé toutes les paires !');
+                            }, 500);
+                        }
+
+                    } else {
+                        setTimeout(() => {
+                            clickedcard.forEach(card => {
+                                if (!card.img.classList.contains('matched')) {
+                                    card.img.setAttribute('src', visuel);
+                                }
+                            });
+                            clickedcard = [];
+                            // img.addEventListener('click',c)
+
+                        }, 1000)
+                    }
+                }
+
+
+
+
+
+            })
+        }) // fin fonction link//
+};
+
+//     img.setAttribute('src', visuel)
+//     console.log("hello")
+// }
+// else{
+//     clickedcard.forEach(function(card)
+//     {
+//         img.setAttribute('src', visuel)
+//     });
+//     clickedcard = []
+// }
+
+// clickedcard[0].img.setAttribute('src', visuel);
+//                     clickedcard[1].img.setAttribute('src', visuel);
